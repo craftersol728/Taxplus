90b12424a838f72fa6fecf9ddf9fe1d643b707db
Jerry Wan <craftersol728@gmail.com>
2023-05-22 19:46:13 -0700

    re adjusted workding

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -19,7 +19,7 @@ const Hero = () => {
                             {/* We build digital solutions to help businesses scale */}
                             Helping Clients Keep More Of What They Earn
                             </h1>
-                            <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We are a team of highly motivated and skilled CPA & Bookkeepers dedicated to delivering only the best outcome.</div>
+                            <div className="text-xl font-semibold tracking-tight mb-5 text-gray-500">We are a team of highly motivated and skilled EA & Bookkeepers dedicated to delivering only the best outcome.</div>
                             <div className="mb-4 space-x-0 md:space-x-2 md:mb-8">
                                 <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-3 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0">
                                     Learn more
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -22,9 +22,7 @@ const Intro = () => {
                                 <p className='my-3 text-xl text-gray-600 font-semibold'>We keep abreast of tax law changes throughout the year to ensure that you are taking advantage of ALL tax incentives.</p>
                             </div>
                             
-                            <div>
-                                <p className='my-3 text-xl text-gray-600 font-semibold'>We take responsibility for building custom software solutions that caters for automation of your business processes and improve efficiency.</p>
-                            </div>
+                        
                             <Link to="/contact" className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-full px-6 py-2 my-4 text-lg shadow-xl rounded-2xl sm:w-auto sm:mb-0 group">
                                 Contact us
                                 <svg className="w-4 h-4 ml-1 group-hover: translate-x-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>

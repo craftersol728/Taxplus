650e9bddcd1df9e7f4da8e4279cebae11557f3da
Jerry Wan <craftersol728@gmail.com>
2023-05-22 19:51:55 -0700

    ad made logo larger

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -24,7 +24,7 @@ const NavBar = () => {
         <nav className={`fixed top-0 w-full z-30 transition duration-300 ease-in-out mb-16 ${!top && 'bg-white shadow-lg'}`}>
             <div className="flex flex-row justify-between items-center py-2">
                 <div className="flex flex-row justify-center md:px-12 md:mx-12 items-center text-center font-semibold">
-                <Link to="/"><img src={logo} alt="TaxPlus logo" className="h-12 w-auto" /></Link>
+                <Link to="/"><img src={logo} alt="TaxPlus logo" className="h-16 w-auto" /></Link>
                     
                 </div>
                 <div className="group flex flex-col items-center">

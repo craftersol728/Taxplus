7d804c4c59c60ed37c2c29cc522d9e71234472f2
Jerry Wan <craftersol728@gmail.com>
2023-06-05 20:40:42 -0700

    deleted demo page

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -13,11 +13,9 @@ const NavLinks = () => {
             </HashLink>
             <a className="px-4 font-extrabold text-gray-500 hover:text-blue-900" href="https://exchange.leapfile.com/fts/drop/custom/Index.jsp">File Transfer</a>
 
-            <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact">
+
+            <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/contact">
                 Contact Us
-            </HashLink>
-            <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo">
-                Demo our products
             </Link>
         </>
     )
